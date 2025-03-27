a4fe0f0755881a4ccf89c21fe5912c81788c52c9
hanin2 <haninelkholy@gmail.com>
2025-03-27 03:58:01 +0200

    Updated request_bot.py and removed old requirements.txt.save

--- request_bot.py
+++ request_bot.py
@@ -12,6 +12,12 @@ print("TOKEN =", TOKEN)
 from telebot import TeleBot
 bot = TeleBot(TOKEN)
 app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
 
 user_data = {}
 
